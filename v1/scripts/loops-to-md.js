9d74be5760340cdf13f1a7f6c75db0c39bab8189
danigb <danigb@gmail.com>
2017-04-28 23:51:04 +0200

    Antropoloops metadata v1

---
+++ v1/scripts/loops-to-md.js
@@ -0,0 +1,55 @@
+const loops = require("../BDloops.json");
+
+const fields = Object.keys(loops[0]);
+const songs = loops.reduce((songs, loop) => {
+  const name = loop.antropoloop;
+  if (!songs[name]) songs[name] = [];
+  songs[name].push(loop);
+  return songs;
+}, {});
+const songNames = Object.keys(songs);
+
+const each = (list, fn) => list.map(fn).join("");
+
+// Components
+
+const tableHead = fields =>
+  `
+|${fields.join("|")}|
+|${fields.map(() => " --- ").join("|")}|`;
+
+const tableBody = (fields, rows) =>
+  each(rows, row => "|" + fields.map(field => row[field]).join("|") + "|\n");
+
+const Metadata = `
+Number of loops: ${loops.length}
+Fields: ${fields.join(" | ")}
+`;
+
+const SongItem = name =>
+  `
+- ${name} (${songs[name].length} loops)`;
+
+const Song = name => {
+  const song = songs[name];
+  return `
+## ${name}
+
+Number of loops: ${song.length}
+
+${tableHead(fields)}
+${tableBody(fields, song)}
+`;
+};
+
+const README = `
+# Loops metadata
+${Metadata}
+
+
+## Songs
+${each(songNames, SongItem)}
+${each(songNames, Song)}
+`;
+
+process.stdout.write(README);
