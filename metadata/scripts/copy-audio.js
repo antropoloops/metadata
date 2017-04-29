d631e15ad76920ae9506b4bc9ffbb735e9a33f9b
danigb <danigb@gmail.com>
2017-04-29 02:11:24 +0200

    Move loops to its own directory

--- metadata/scripts/copy-audio.js
+++ metadata/scripts/copy-audio.js
@@ -2,27 +2,18 @@ const shell = require("shelljs");
 const path = require("path");
 
 const from = path.join(__dirname, "../../raw/loops/");
-const to = path.join(__dirname, "../../lik/");
-const fullPath = filename => path.join(from, filename);
+const to = path.join(__dirname, "../../lik/loops");
+shell.mkdir(to)
 
 const found = require("../../lik/missing.json").found;
 
-const convert = file => {
-  const input = fullPath(file.filename);
+const convert = (file, i, arr) => {
+  const input = path.join(from, file.filename);
   const output = path.join(to, file.matched + ".mp3");
   const cmd = `lame -V0 --resample 44100 "${input}" ${output}`;
   if (shell.exec(cmd)) {
-    console.log("Done!");
+    console.log(`DONE ${i}/${arr.length}`)
   }
 };
+console.log(found.length)
 found.slice(0, 10).map(convert);
-
-const print = () => {
-  found.forEach(data => {
-    const full = fullPath(data.filename);
-    if (fs.existsSync(full)) {
-      console.log("PATH", full);
-      console.log("FILE", data.matched);
-    }
-  });
-};
